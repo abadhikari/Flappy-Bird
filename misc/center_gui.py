2c80f22fb4d0a62a60b4d3dfb41cca4074a33c71
Abhinna Adhikari <adhikari.abhinna@gmail.com>
2019-11-13 00:48:42 -0500

    Center the gui to screen better

--- misc/center_gui.py
+++ misc/center_gui.py
@@ -5,6 +5,6 @@ import os
 def center_screen(gui_dim):
     # centers the pygame gui in the middle of the screen
     screensize = ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1)
-    x, y = screensize[0] // 2 - gui_dim[0] // 2, (screensize[1] - gui_dim[1]) // 2
+    x, y = screensize[0] // 2 - gui_dim[0] // 2, screensize[1] - gui_dim[1] 
     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x, y)
 
